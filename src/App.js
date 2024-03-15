3253d7be5acded91f8586d10a80d5a040a832b60
alfisyar <54832784+alfisyar520@users.noreply.github.com>
2024-03-15 11:11:01 +0700

    add(feature): add section about

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ function App() {
                   Alfisyar Jefry Pranata
                 </span>
               </h1>
-              <h2 className="font-medium text-slate-500 text-lg mb-5 mt-1 lg:text-2xl">
+              <h2 className="font-medium text-secondary text-lg mb-5 mt-1 lg:text-2xl">
                 Frontend Developer
               </h2>
-              <p className="font-medium text-slate-400 mb-10 leading-relaxed">
+              <p className="font-medium text-secondary mb-10 leading-relaxed">
                 I’am interested in programming because it can help someone to
                 solve a problem.
               </p>
@@ -56,6 +56,30 @@ function App() {
       </section>
 
       {/* about section */}
+      <section id="about" className="pt-36 pb-32">
+        <div className="container">
+          <div className="flex flex-wrap">
+            <div className="w-full px-4 mb-10 lg:w-1/2">
+              <h4 className="font-bold uppercase text-primary text-lg mb-3">Tentang Saya</h4>
+              <h2 className="font-bold text-dark text-3xl mb-3 max-w-md lg:text-4xl">Yuk, Kenalan dengan saya</h2>
+              <p className="font-medium text-base text-secondary max-w-xl lg:text-lg">lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
+            </div>
+            <div className="w-full px-4 lg:w-1/2">
+              <h3 className="font-semibold text-dark text-2xl mb-4 lg:text-3xl lg:pt-10">Mari berteman</h3>
+              <p className="font-medium text-base text-secondary mb-6 lg:text-lg">lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
+              <div className="flex items-center">
+                <a href="https://www.linkedin.com/in/alfisyar-jefry-pranata-668833193/" target="_blank" className="w-9 h-9 mr-3 rounded-full flex justify-center items-center border border-slate-300 hover:border-primary hover:bg-primary hover:text-white text-slate-300">
+                  <svg className="fill-current" role="img" width={20} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>LinkedIn</title><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" /></svg>
+                </a>
+                <a href="https://medium.com/@alfisyarpranata" target="_blank" className="w-9 h-9 mr-3 rounded-full flex justify-center items-center border border-slate-300 hover:border-primary hover:bg-primary hover:text-white text-slate-300">
+                  <svg className="fill-current" role="img" width={20} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Medium</title><path d="M13.54 12a6.8 6.8 0 01-6.77 6.82A6.8 6.8 0 010 12a6.8 6.8 0 016.77-6.82A6.8 6.8 0 0113.54 12zM20.96 12c0 3.54-1.51 6.42-3.38 6.42-1.87 0-3.39-2.88-3.39-6.42s1.52-6.42 3.39-6.42 3.38 2.88 3.38 6.42M24 12c0 3.17-.53 5.75-1.19 5.75-.66 0-1.19-2.58-1.19-5.75s.53-5.75 1.19-5.75C23.47 6.25 24 8.83 24 12z" /></svg>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
     </div>
   );
 }
