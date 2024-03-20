b9b0592141a556144941013b8e726fb4fea5b65a
alfisyar <54832784+alfisyar520@users.noreply.github.com>
2024-03-20 14:56:55 +0700

    update(portofolio): update portofolio

---
+++ src/utils/constant.js
@@ -0,0 +1,17 @@
+import baymaksImage from "../assets/portofolio/Baymaks/Web Publik.png";
+import botImage from "../assets/portofolio/bot telegram/Screenshot_20210817_153718.jpg";
+
+export const ListPortofolio = [
+    {
+        title: 'HALO DOKTER',
+        image: botImage,
+        link: 'https://medium.com/@alfisyarpranata/halo-dokter-dca50f602bce',
+        description: 'Halo Dokter merupakan sebuah aplikasi yang digunakan untuk menentukan penyakit mata menggunakan metode Fuzzy Tsukamoto. Aplikasi ini akan membantu menganalisis apakah mata penggunanya mengalami sakit mata atau tidak dengan cara mengisi beberapa parameter yang disediakan. Teknologi yang dipakai pada aplikasi ini adalah dengan menggunakan Java untuk membuat aplikasi mobile. Aplikasi ini dikerjakan pada November 2019 sampai Desember 2019 dengan 3 pengembang di tim saya.\n Kontribusi saya dalam proyek ini adalah sebagai pengembang aplikasi mobile, membuat User Interface dengan Java, kemudian di integrasiikan dengan algoritma Fuzzy Tsukamoto.\n Fitur-fitur aplikasi ini adalah dashboard, cek parameter Kesehatan, hasil diagnosa.',
+    },
+    {
+        title: 'FITPACK',
+        image: baymaksImage,
+        link: 'https://medium.com/@alfisyarpranata/fitpack-d71174010573',
+        description: 'FITPACK (Fluor albus IndicaTor Test Pack) merupakan aplikasi yang digunakan berdasarkan untuk mendeteksi dini keputihan pada wanita. Aplikasi ini akan membantu menerjemahkan hasil deteksi dari alat FITPACK dengan menampilkan warna dan bau dengan dihubungkan melalui Bluetooth. Teknologi yang dipakai pada aplikasi ini adalah dengan menggunakan Java untuk membuat aplikasi mobile. Aplikasi ini akan menerima hasil deteksi dari alat deteksi dini keputihan yang dibuat dengan menggunakan Arduino. Aplikasi ini dikerjakan pada Agustus 2020 sampai September 2020 dengan 1 pengembang, yaitu saya seorang.\n Kontribusi saya dalam proyek ini adalah sebagai pengembang aplikasi mobile. Membuat User Interface dengan Java. Kemudian akan disambungkan dengan alat deteksi keputihan melalui Bluetooth.\n Fitur-fitur aplikasi ini adalah dashboard, profile, hasil deteksi keputihan, dll.',
+    },
+]
